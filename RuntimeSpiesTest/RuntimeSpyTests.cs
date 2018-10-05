4bcfe5c629c89381fe36adc8c9e7e414b16605d2
Yaki <yaki.koren@gmail.com>
2018-10-05 16:53:54 +0300

    runtime spy returns ptial harness

---
+++ RuntimeSpiesTest/RuntimeSpyTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RuntimeSpiesTest
+{
+    [TestClass]
+    public class RuntimeSpyTests
+    {
+        private string testFunction(int a, string b)
+        {
+            var mySpy = new RuntimeSpy();
+            mySpy.SetMethodCall(MethodBase.GetCurrentMethod(), a, b);
+
+            return mySpy.getHarness();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("a = 23 ;\nb = \"twenty three\" ;\n\ntestFunction(a, b);\n", testFunction(23,"twenty three"));
+        }
+    }
+}
