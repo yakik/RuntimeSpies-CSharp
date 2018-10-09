997e737fe37954e59c0fc30aca8932f575e839cf
Yaki <yaki.koren@gmail.com>
2018-10-09 12:33:53 +0300

    Added RuntimeSpySequence

---
+++ RuntimeSpies/RuntimeSpySequence.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimeSpies
+{
+    public sealed class RuntimeSpySequence
+    {
+        private int _sequence = -1;
+
+        public int _getSequence()
+        {
+            _sequence++;
+            return _sequence;
+            
+        }
+
+        public void _Reset()
+        {
+            _sequence = -1;
+        }
+
+        private RuntimeSpySequence()
+        {
+        }
+
+        public static int GetSequence()
+        {
+            return Nested.instance._getSequence();
+        }
+
+        private class Nested
+        {
+            // Explicit static constructor to tell C# compiler
+            // not to mark type as beforefieldinit
+            static Nested()
+            {
+            }
+
+            internal static readonly RuntimeSpySequence instance = new RuntimeSpySequence();
+        }
+
+        public static void Reset()
+        {
+            Nested.instance._Reset();
+        }
+    }
+}
---
+++ RuntimeSpiesTest/RuntimeSpySequenceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RuntimeSpies;
+
+namespace RuntimeSpiesTest
+{
+    [TestClass]
+    public class RuntimeSpySequenceTests
+    {
+        [TestMethod]
+        public void TestSequence()
+        {
+            RuntimeSpySequence.Reset();
+            Assert.AreEqual(0,RuntimeSpySequence.GetSequence());
+            Assert.AreEqual(1,RuntimeSpySequence.GetSequence());
+            Assert.AreEqual(2,RuntimeSpySequence.GetSequence());
+        }
+        [TestMethod]
+        public void TestSequence2()
+        {
+            RuntimeSpySequence.Reset();
+            Assert.AreEqual(0, RuntimeSpySequence.GetSequence());
+            Assert.AreEqual(1, RuntimeSpySequence.GetSequence());
+            Assert.AreEqual(2, RuntimeSpySequence.GetSequence());
+        }
+    }
+}
