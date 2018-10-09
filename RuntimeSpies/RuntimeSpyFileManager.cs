48aeb960a505b5b615046e963fe5860a4eac7c80
Yaki <yaki.koren@gmail.com>
2018-10-09 15:17:27 +0300

    RuntimeSpyFileManager works

---
+++ RuntimeSpies/RuntimeSpyFileManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq.Expressions;
+
+namespace RuntimeSpies
+{
+    public class RuntimeSpyFileManager
+    {
+        public static string GetTestFileCode(string fileName)
+        {
+            try
+            {
+                string allFileContent = File.ReadAllText(fileName);
+                return allFileContent;
+            }
+            catch (IOException)
+            {
+                return "EMPTY";
+            }
+        }
+
+        public static void UpdateTestFile(string fileName, string code)
+        {
+            File.WriteAllText(fileName, code);
+        }
+    }
+}
---
+++ RuntimeSpiesTest/RuntimeSpyUnitTestGeneratorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RuntimeSpies;
+
+namespace RuntimeSpiesTest
+{
+    [TestClass]
+    public class RuntimeSpyUnitTestGeneratorTests
+    {
+
+        [TestMethod]
+        public void CreatingTestClass()
+        {
+            RuntimeSpySequence.Reset();
+            string unitTestMethodCode = "var i = 3;";
+            Assert.AreEqual("using System;\nusing Microsoft.VisualStudio.TestTools.UnitTesting;\n" +
+                            "\nnamespace myNamespace{\n\n" +
+                            "[TestClass]\npublic class myTestClassName\n{\n" +
+                "[TestMethod]\npublic void myTestName0()\n{\nvar i = 3;\n}\n\n}\n}",
+                RuntimeSpyUnitTestGenerator.AddCodeAsUnitTestToTestClass(unitTestMethodCode, "myTestName", "myTestClassName", "myNamespace", ""));
+        }
+
+        [TestMethod]
+        public void AddingTestMethodToATestClass()
+        {
+            RuntimeSpySequence.Reset();
+            string myTestClass = RuntimeSpyUnitTestGenerator.AddCodeAsUnitTestToTestClass("var i = 3;", "myTestName", "myTestClassName", "myNamespace", "");
+            string unitTestMethodCode = "var i = 3;";
+            Assert.AreEqual("using System;\nusing Microsoft.VisualStudio.TestTools.UnitTesting;\n" +
+                            "\nnamespace myNamespace{\n\n" +
+                            "[TestClass]\npublic class myTestClassName\n{\n" +
+                            "[TestMethod]\npublic void myTestName0()\n{\nvar i = 3;\n}\n\n"+
+                            "[TestMethod]\npublic void myTestName1()\n{\nvar i = 3;\n}"+
+                            "\n\n}\n}",
+                RuntimeSpyUnitTestGenerator.AddCodeAsUnitTestToTestClass(unitTestMethodCode, "myTestName", "myTestClassName", "myNamespace", myTestClass));
+        }
+    }
+}
